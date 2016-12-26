9d26b89e6fa7154b685514c3ba196c21a7a42ca4
Hamish Rickerby <hamish@simplemachines.com.au>
2016-12-26 12:37:25 +1100

    Day 6, part 1

--- AoC2016/app/Main.hs
+++ AoC2016/app/Main.hs
@@ -5,6 +5,7 @@ import qualified AoC201602 as AoC02
 import qualified AoC201603 as AoC03
 import qualified AoC201604 as AoC04
 import qualified AoC201605 as AoC05
+import qualified AoC201606 as AoC06
 
 main :: IO ()
 main = do
@@ -13,3 +14,4 @@ main = do
   AoC03.runDay
   AoC04.runDay
   AoC05.runDay
+  AoC06.runDay
