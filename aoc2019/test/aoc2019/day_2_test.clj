95fe1810d4caa8e20356adf6898152da2037352d
Hamish Rickerby <hamish@happtic.com>
2019-12-04 21:44:04 +1100

    Fix tests for day 2

--- aoc2019/test/aoc2019/day_2_test.clj
+++ aoc2019/test/aoc2019/day_2_test.clj
@@ -4,7 +4,7 @@
 
 (deftest part-1-test
   (testing "Examples for part 1 work"
-    (is (= [2 0 0 0 99] (sut/run-program [1 0 0 0 99])))
-    (is (= [2,3,0,6,99] (sut/run-program [2,3,0,3,99])))
-    (is (= [2,4,4,5,99,9801] (sut/run-program [2,4,4,5,99,0])))
-    (is (= [30,1,1,4,2,5,6,0,99] (sut/run-program [1,1,1,4,99,5,6,0,99])))))
+    (is (= 2(sut/run-program [1 0 0 0 99])))
+    (is (= 2 (sut/run-program [2,3,0,3,99])))
+    (is (= 2 (sut/run-program [2,4,4,5,99,0])))
+    (is (= 30 (sut/run-program [1,1,1,4,99,5,6,0,99])))))
